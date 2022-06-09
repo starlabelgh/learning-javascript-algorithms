d779a0e37a2acd0d82e47f9bf5fcc80283c53a3e
starlabelgh <hello@starlabel.co>
2022-06-09 19:36:11 +0000

    using js algorithm to reverse a srting

---
+++ reversewords.js
@@ -0,0 +1,27 @@
+//how to reverse a string of words
+
+//define the function reverseWords passing a string parameter
+function reverseWords(string){
+    //create an array of all the words in our string
+    var wordsArr = string.split(' ');
+
+    //define an empty array to put all our reverse words into after reversing them
+    var reversedWordsArr = [];
+
+    //loop through our reverse words
+    wordsArr.forEach(word => {
+        var reversedWords = '';
+
+        //looping every letter in our words backwards
+        for (var i = word.length -1; i >= 0; i--) {
+            reversedWords += word[i];
+        }
+
+        reversedWordsArr.push(reversedWords);
+    });
+
+    return reversedWordsArr.join(' ');
+}
+
+
+reverseWords('my name is samanhyia moses, i code like a boss')
