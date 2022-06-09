0bf1d4a3a528e96f4ba8483957149f10f0e25954
starlabelgh <hello@starlabel.co>
2022-06-09 18:48:50 +0000

    added comments to the code

--- caesar-cipher.js
+++ caesar-cipher.js
@@ -1,3 +1,9 @@
+// The objective of the Caesar Ciphar is to shift every letter (string) in a given String
+// by the number (num) that is passed in. So for example, if we pass in the string 'Moses' 
+// and the number 2, it should return the string Oqugu. Every letter in the string shifted
+// two places in the alphabet.
+
+//create the function caesarCiphar in camelCase passing the two parameters
 function caesarCiphar (str, num){
     num = num % 26;
     var lowerCaseString = str.toLowerCase();
