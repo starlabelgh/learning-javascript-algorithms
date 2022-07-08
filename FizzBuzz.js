59236026eba3e7be34b6e0e1bd60b913826ca969
starlabel <hello@starlabel.co>
2022-07-08 21:38:18 +0000

    fizzbuzz code to determine is a number is divible by 3 or 5 and print fizz or buzz or fizzbuzz

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,10 +1,24 @@
+//how to print fizz or buzz or fizzbuzz when a number is generated is divisible by 3
+
 function fizzBuzz(num){
     for (var i = 1; i <= num; i++){
-        if (i % 15 === 0 ) console.log('FizzBuzz');
-        else if (i % 3 === 0) console.log('Fizz');
-        else if (i % 5 === 0) console.log('Buzz');
-        else console.log(i);
+      if(i%3===0 && i%5===0)console.log('FizzBuzz')
+      else if(i%3 ===0)consoe.log('Fizz')
+      else if(i%5 ===0)console.log('Buzz')
+      else console.log(i)
     }
-}
-
-fizzBuzz(30);
+  }
+  
+  fizzBuzz(30)
+  
+  //refacted fizzbuzz
+  function fizzBuzz(num){
+    for (var i = 1; i <= num; i++){
+      if(i%15 ===0)console.log('FizzBuzz')
+      else if(i%3 ===0)consoe.log('Fizz')
+      else if(i%5 ===0)console.log('Buzz')
+      else console.log(i)
+    }
+  }
+  
+  fizzBuzz(30)
